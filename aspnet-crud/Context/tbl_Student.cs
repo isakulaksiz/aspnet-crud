537dd5e73579ba30d3273d3f1dfc3434cfb46fea
isakulaksiz <isakulaksiz@outlook.com>
2020-10-08 16:34:01 +0300

    annotations and error message added

--- aspnet-crud/Context/tbl_Student.cs
+++ aspnet-crud/Context/tbl_Student.cs
@@ -11,13 +11,20 @@ namespace aspnet_crud.Context
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
     public partial class tbl_Student
     {
         public int ID { get; set; }
+        [Required(ErrorMessage ="Required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Required")]
         public string Fname { get; set; }
+        [Required(ErrorMessage = "Required")]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Required")]
+        [MinLength(11, ErrorMessage ="Mobile no should be 11 digit")]
         public string Mobile { get; set; }
         public string Description { get; set; }
     }
